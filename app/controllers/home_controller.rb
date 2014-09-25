4f0f7f20f850d08e2c6e2bae90661ecdc57d9061
Yusuke Ohashi <yusuke@junkpiano.me>
2014-09-26 02:24:41 +0900

    fix envitornmental variables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 
 class HomeController < ApplicationController
   def index
-    url = "https://app.rakuten.co.jp/services/api/Kobo/EbookSearch/20140811?format=json&keyword=rails&affiliateId=131e0f83.befaf501.131e0f84.ef6d0def&sort=sales&applicationId=" + ENV[RAKUTEN_API_KEY]
+    url = "https://app.rakuten.co.jp/services/api/Kobo/EbookSearch/20140811?format=json&keyword=rails&affiliateId=131e0f83.befaf501.131e0f84.ef6d0def&sort=sales&applicationId=" + ENV["RAKUTEN_API_KEY"]
     raw = open(url)
     data = JSON.parse(raw.read)
     @result = data["Items"]
